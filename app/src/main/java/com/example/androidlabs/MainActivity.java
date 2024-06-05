3189a5c670e3c2054a2d723594204a0d681e6bbb
Natalia Ricciardi <ricciardi.nsera@gmail.com>
2024-06-05 15:41:05 -0400

    Lab 3 commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,45 +13,70 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.snackbar.Snackbar;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edt;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        final Button btn = findViewById(R.id.pressbtn);
-        final TextView txt = findViewById(R.id.text);
-        final EditText edt = findViewById(R.id.editbox);
+        // initialize button
+        final Button nextbtn = findViewById(R.id.nextBtn);
+        // initialize edittext
+        edt = findViewById(R.id.textBox);
 
-        btn.setOnClickListener(
-                (click) -> {
-                    txt.setText(edt.getText().toString());
-                    String message = getResources().getString(R.string.toast_message);
-                    Toast.makeText(MainActivity.this,  message, Toast.LENGTH_SHORT).show();
-                }
+        // Load data from sharedPreferences and put in edittext if it's not empty
+        prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String savedText = prefs.getString("savedmessage", "");
+        if (!savedText.isEmpty()) {
+            edt.setText(savedText);
+        }
+
+        // initialize intent
+        Intent nextPage = new Intent(MainActivity.this, NameActivity.class);
+
+        nextbtn.setOnClickListener(
+            (click) -> {
+
+                // onclick next button open next page
+                String message = edt.getText().toString();
+                nextPage.putExtra("savedmessage", message);
+                startActivityForResult(nextPage, 101);
+            }
         );
-        CheckBox cb = findViewById( R.id.check );
-        cb.setOnCheckedChangeListener( (compoundButton, b) -> {
-            String message;
-            if (b) {
-                message = getResources().getString(R.string.checkbox_on);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("savedmessage", edt.getText().toString());
+        editor.apply();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101) {
+            if (resultCode == 0) {
+                // user can enter different name
+                edt.setText("");
             }
-            else {
-                message = getResources().getString(R.string.checkbox_off);
+            else if (resultCode == 1) {
+                // finish and close
+                finish();
             }
-            Snackbar.make(cb,getResources().getString(R.string.snackbar_message) + " " + message, Snackbar.LENGTH_LONG)
-                    .setAction("Undo", click -> cb.setChecked(!b))
-                    .show();
-        });
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidlabs/NameActivity.java
@@ -0,0 +1,54 @@
+package com.example.androidlabs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class NameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_name);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        final TextView welcome = findViewById(R.id.textThing);
+        String message = intent.getStringExtra("savedmessage");
+        assert message != null;
+        if (!message.isEmpty()) {
+            String welcomefren = getResources().getString(R.string.welcome2);
+            welcome.setText(welcomefren + " " + message + "!");
+        }
+
+        final Button dontcall = findViewById(R.id.button1);
+        final Button thank = findViewById(R.id.button2);
+
+        dontcall.setOnClickListener(
+            (click) -> {
+                setResult(0);
+                finish();
+            }
+        );
+        thank.setOnClickListener(
+            (click) -> {
+                setResult(1);
+                finish();
+            }
+        );
+    }
+}
