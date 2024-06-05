6501f94eda275c3e7b65f4e8792e6d095dc9d19e
Natalia Ricciardi <ricciardi.nsera@gmail.com>
2024-06-05 16:25:21 -0400

    Lab3 - Deleted unused import in NameActivity.java

--- app/src/main/java/com/example/androidlabs/NameActivity.java
+++ app/src/main/java/com/example/androidlabs/NameActivity.java
@@ -11,8 +11,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Objects;
-
 public class NameActivity extends AppCompatActivity {
 
     @Override
