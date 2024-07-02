8332e86567afc420a1553e5b0a5d0af69d9eea10
Natalia Ricciardi <ricciardi.nsera@gmail.com>
2024-07-02 00:18:06 -0400

    Lab_7 - Initial, unfinished. Runs and loads listview but doesn't show fragments

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.androidlabs;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,16 +18,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private JSONArray characters;
+    private CharAdapter charAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,96 +42,132 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        startCatImagesTask();
+        new SWInfo().execute("https://swapi.dev/api/people/?format=json");
+
+        ListView listview = findViewById(R.id.listview);
+
+        listview.setOnItemClickListener(
+                (parent, view, position, id) -> {
+                    try {
+                        JSONObject character = characters.getJSONObject(position);
+                        String name = character.getString("name");
+                        String height = character.getString("height");
+                        String mass = character.getString("mass");
+
+                        FrameLayout frame = findViewById(R.id.frame);
+
+                        if (frame == null) {
+                            Intent intent = new Intent(MainActivity.this, EmptyActivity.class);
+                            intent.putExtra("name", name);
+                            intent.putExtra("height", height);
+                            intent.putExtra("mass", mass);
+                            startActivity(intent);
+                        }
+                        else {
+                            DetailsFragment fragment = DetailsFragment.newInstance(name, height, mass);
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.frame, fragment)
+                                    .commit();
+                        }
+                    }
+                    catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
 
     }
 
-    private void startCatImagesTask() {
-        ImageView catImageView = findViewById(R.id.catImageView);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
-        new CatImages(catImageView, progressBar).execute();
-    }
-}
 
-class CatImages extends AsyncTask<String, Integer, String> {
+    private class SWInfo extends AsyncTask<String, Integer, String> {
 
-    private ImageView imageView;
-    private Bitmap catimg;
-    private ProgressBar progressBar;
-
-    public CatImages(ImageView imageView, ProgressBar progressBar) {
-        this.imageView = imageView;
-        this.progressBar = progressBar;
-    }
-
-    @Override
-    protected String doInBackground(String... strings) {
-
-        while (true) {
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = "";
             try {
-                URL url = new URL("https://cataas.com/cat?json=true");
+                URL url = new URL(strings[0]);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
 
                 InputStream inputStream = new BufferedInputStream(connection.getInputStream());
-                StringBuilder response = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
                 int byteRead;
 
                 while ((byteRead = inputStream.read()) != -1) {
-                    response.append((char) byteRead);
+                    sb.append((char) byteRead);
                 }
                 connection.disconnect();
 
-                JSONObject jsonResponse = new JSONObject(response.toString());
-                String imageUrl = "https://cataas.com/cat/" + jsonResponse.getString("_id");
-                String imageId = jsonResponse.getString("_id");
+                response = sb.toString();
 
-                // check if exists
-                File imageFile = new File(imageView.getContext().getExternalFilesDir(null), imageId + ".jpg");
-                if (imageFile.exists()) {
-                    catimg = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-                }
-                else {
-                    URL imageDownloadUrl = new URL(imageUrl);
-                    HttpURLConnection imageConnection = (HttpURLConnection) imageDownloadUrl.openConnection();
-                    imageConnection.setRequestMethod("GET");
-                    InputStream imageInputStream = new BufferedInputStream(imageConnection.getInputStream());
+                characters = new JSONObject(response).getJSONArray("results");
 
-                    // Save the image
-                    FileOutputStream fileOutputStream = new FileOutputStream(imageFile);
-                    byte[] buffer = new byte[1024];
-                    int bufferLength;
-                    while ((bufferLength = imageInputStream.read(buffer)) > 0) {
-                        fileOutputStream.write(buffer, 0, bufferLength);
-                    }
-                    fileOutputStream.close();
-                    imageInputStream.close();
-
-                    catimg = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-                }
-
-                for (int i = 0; i < 100; i++) {
-                    try {
-                        publishProgress(i);
-                        Thread.sleep(30);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-
-
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 throw new RuntimeException(e);
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (characters != null) {
+                charAdapter = new CharAdapter(MainActivity.this, characters);
+                ListView listView = findViewById(R.id.listview);
+                listView.setAdapter(charAdapter);
             }
         }
     }
 
-    @Override
-    protected void onProgressUpdate(Integer... values) {
-        super.onProgressUpdate(values);
-        if (catimg != null) {
-            imageView.setImageBitmap(catimg);
+    private class CharAdapter extends BaseAdapter {
+
+        private final Context context;
+        private final JSONArray characters;
+
+        public CharAdapter(Context context, JSONArray characters) {
+            this.context = context;
+            this.characters = characters;
         }
-        progressBar.setProgress(values[0]);
+
+        @Override
+        public int getCount() {
+            return characters.length();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            try {
+                return characters.getJSONObject(position);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.list_items, parent, false);
+            }
+
+            TextView box = convertView.findViewById(R.id.items);
+
+            try {
+                JSONObject character = characters.getJSONObject(position);
+                box.setText(character.getString("name"));
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            return convertView;
+        }
     }
 }
+
